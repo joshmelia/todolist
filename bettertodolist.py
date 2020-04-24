7aa78613fdacc19ec0855c585579a01be86adc27
joshmelia <64174690+joshmelia@users.noreply.github.com>
2020-04-25 00:23:50 +1000

    added username and ability to remove list items

--- bettertodolist.py
+++ bettertodolist.py
@@ -2,79 +2,97 @@ import json
 import os
 from sys import platform
 
-def cl():
-    if platform == 'win32':
-        return os.system('cls')
-    else:
-        return os.system('clear')
+user = input("Enter your name: ").upper()
 
-cl()
+def listgen():
+    def cl():
+        if platform == 'win32':
+            return os.system('cls')
+        else:
+            return os.system('clear')
 
-todo = []
+    cl()
 
-try: 
-    with open('mylist.txt', 'r') as f:
-        todo = json.load(f)
+    todo = []
 
-except:
-    open('mylist.txt','w').close()
+    try: 
+        with open('mylist.txt', 'r') as f:
+            todo = json.load(f)
 
-print("Welcome to your To-do List!!\n")
-
-if not todo==[]:
-    print("Today we are doing:\n") 
-    for i,task in enumerate(todo):
-        print(f"{i+1}. {task}")
-
-print("\nOnce you have finished adding to your list, type 'finish'\nTo clear your list, just type 'clear'.\n")
-
-while True:
-    a = input("What are you doing today? ")
-    if a.lower() == "finish":
-        break
-    elif a.lower() == "clear":
+    except:
         open('mylist.txt','w').close()
-        todo = []
-        cl()
-        print("List cleared.")
-    elif a.lower() == "exit":
-        print("Thanks for choosing JOSHY'S LISTS")
-        input()
-        exit() 
-    else:
-        todo += [a]
 
-def betterclear(message):
-    def clearscreen(func):
-        def wrap():
+    print(f"Welcome to your To-do List, {user}!!\n")
+
+    if todo==[]:
+        print("There's nothing in your list yet!")
+    else:
+        print("Today we are doing:\n") 
+        for i,task in enumerate(todo):
+            print(f"{i+1}. {task}")
+
+    print("\nOnce you have finished adding to your list, type 'finish'\nTo clear your list, just type 'clear'.")
+    print("To exit, simply type 'exit'.\n\n")
+
+    while True:
+        a = input("What are you doing today? ")
+        if a.lower() == "finish":
+            break
+        elif a.lower() == "clear":
+            open('mylist.txt','w').close()
+            todo = []
             cl()
-            print(f"---------  {message}  ---------\n")
-            func()
-            print(f"\n---------  {message}  ---------")
-        return wrap
-    return clearscreen
+            print("List cleared.")
+        elif a.lower() == "exit":
+            print(f"Thanks for coming, {user}! Press enter to close the window.")
+            input()
+            exit() 
+        else:
+            todo += [a]
 
-@betterclear("JOSHY'S LIST")
-def showlist():
-    while("" in todo):
-        todo.remove("")
-  
-    if not todo == []:
-        print("Today we will be doing:\n") 
-        for i,task in enumerate(todo,1):
-           print(f"{i}. {task}")
-    else:
-        print("There's nothing in your list!")
+    def betterclear(message):
+        def clearscreen(func):
+            def wrap():
+                cl()
+                print(f"---------  {message}  ---------\n")
+                func()
+            return wrap
+        return clearscreen
 
-showlist()
+    def rem():
+            itemno = input("\n\nDo you want to remove an item? (type 'yes' or 'no'): ")
+            if itemno.lower() == "yes":
+                gone = int(input("Type the number of the item to be removed: ")) - 1
+                try:
+                    todo.pop(gone)
+                except:
+                    print("\nPick something from the list!")
+                    rem()
+                with open('mylist.txt', 'w') as f:
+                    json.dump(todo, f)
+                showlist()
+            else:
+                with open('mylist.txt', 'w') as f:
+                    json.dump(todo, f)
+                listgen()
 
-with open('mylist.txt', 'w') as f:
-    json.dump(todo, f)
-
-
-input()
-
-
+    @betterclear(f"{user}'S LIST")
+    def showlist():
+        while("" in todo):
+            todo.remove("")
+        if todo == []:
+            print("There's nothing in your list!")
+            input("\nPress Enter to continue...")
+            listgen()
+        else:
+            print("Today we will be doing:\n") 
+            for i,task in enumerate(todo,1):
+                print(f"{i}. {task}")
+            rem()
+        
+        
+    showlist()
+listgen()
     
 
 
